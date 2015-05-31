b53934f87118b75b2292e37897fbd6e6b68ff4a6
yosqueoy <yosukeoyama@gmail.com>
2015-05-31 11:43:33 +0800

    initial commit

---
+++ ZabbixSenderNet/ZabbixSender.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ysq.Zabbix
+{
+    public class Sender
+    {
+        private int _port;
+        private string _zabbixServer;
+
+        public Sender(string zabbixServer, int port = 10051)
+        {
+            _zabbixServer = zabbixServer;
+            _port = port;
+        }
+
+        public SenderResponse Send(string host, string itemKey, string value)
+        {
+            dynamic req = new ExpandoObject();
+            req.request = "sender data";
+            dynamic d = new ExpandoObject();
+            d.host = host;
+            d.key = itemKey;
+            d.value = value;
+            req.data = new[] { d };
+            var jsonReq = JsonConvert.SerializeObject(req);
+            using (var tcpClient = new TcpClient(_zabbixServer, _port))
+            using(var networkStream = tcpClient.GetStream())
+            {
+                var writer = new BinaryWriter(networkStream);
+                writer.Write(jsonReq);
+                writer.Flush();
+                var res = new BinaryReader(networkStream).ReadString();
+                return JsonConvert.DeserializeObject<SenderResponse>(res);
+            }       
+        }
+    }
+}
